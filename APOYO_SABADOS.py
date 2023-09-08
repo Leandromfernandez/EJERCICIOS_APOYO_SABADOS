48e4bc88f1daebfb6a4ecd5ace9de7e9b5eedeea
Leandro Fernández <122303139+Leandromfernandez@users.noreply.github.com>
2023-09-08 15:48:21 -0300

    Add files via upload

--- APOYO_SABADOS.py
+++ APOYO_SABADOS.py
@@ -73,25 +73,39 @@ lista_edades = []
 lista_generos = []
 lista_votos = []
 
+contador_apple = 0
 contador = 0
+
+contador_masculino = 0
+contador_femenino = 0
+contador_otro = 0
+
+contador_no_apple = 0
+
+
+
 while encuesta == "SI":
     # 1.Cargar voto, está opción agregara a las listas un voto en especifico pidiendo los siguientes datos
+    
     #nombre del encuestado.
     nombre = input('Ingrese su nombre: ')
     nombre.capitalize()
     while nombre == "":
         nombre = input('Error. Ingrese su nombre: ')
         nombre.capitalize()
+    
     #edad (no menor a 18)
     edad = int(input('Ingrese su edad: '))
     while edad < 18:
         edad = int(input('Error ingrese una edad mayor de 18 años: '))
+    
     #genero (Masculino - Femenino - Otro)
     genero = input('Ingrese su genero [Masculino - Femenino - Otro]: ')
     genero = genero.lower()
     while genero != "masculino" and genero != "femenino" and genero != "otro":
         genero = input('ERROR. Ingrese su genero [Masculino - Femenino - Otro]: ')
         genero = genero.lower()
+    
     #voto (APPLE, DUNKIN, IKEA, TACO)
     voto = input(f'¿Cual de estas empresas le gustaria en Argentina {empresas}: ')
     voto = voto.upper()
@@ -103,16 +117,42 @@ while encuesta == "SI":
     lista_nombres.append(nombre)
     lista_edades.append(edad)
     lista_generos.append(genero)
-    lista_votos.append(voto)
-        
-    
-    
-    
+    lista_votos.append(voto)       
+   
     contador += 1
     encuesta = input('¿Desea continuar? [SI-NO]: ')
     encuesta = encuesta.upper()
 
+#2-Cantidad de encuestados que votaron por APPLE, cuya edad no supere los 35 años.
+for i in range(len(lista_nombres)):    
+    if lista_votos[i] == "APPLE" and lista_edades[i] < 35: 
+        contador_apple += 1
+#3-Género que predomina en la empresa.
+    if lista_generos[i] == "masculino":
+        contador_masculino += 1
+    elif lista_generos[i] == "femenino":
+        contador_femenino += 1
+    elif lista_generos[i] == "otro":
+        contador_otro += 1
+#4-Porcentaje de empleados que no votaron por APPLE , siempre y cuando su género no sea Femenino o su edad se encuentre 
+# entre los 18 y 30.
+    if lista_votos[i] != "APPLE" and (lista_edades[i] > 17 and lista_edades[i] <=30) and lista_generos[i] != "FEMENINO":
+        contador_no_apple += 1
+        porcentaje_no_apple = (contador * 100) / contador_no_apple
+
+
+print(f'la cantidad que votaron apple menores de 35 son: {contador_apple}')
+if contador_masculino > contador_femenino and contador_masculino > contador_otro:
+    print(f'EL GENERO MASCULINO PREDOMINA EN LA EMPRESA')
+elif contador_femenino > contador_masculino and contador_femenino > contador_otro:
+    print(f'EL GENERO FEMENINO PREDOMINA EN LA EMPRESA')
+elif contador_otro > contador_masculino and contador_otro > contador_femenino:
+    print(f'EL GENERO OTRO PREDOMINA EN LA EMPRESA')
+    
+
+
+print(f'en la empresa hay {contador_masculino} hombres\nhay {contador_femenino} mujeres \ny {contador_otro} otros')
+
 print(f'las listas son:\n nombres: {lista_nombres}\n edades: {lista_edades}\n genero: {lista_generos}\n voto: {lista_votos}')
 
-
-
+print(f'El porcentaje de empleados que no votaron apple es de % {porcentaje_no_apple}')
